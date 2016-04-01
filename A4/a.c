9f7f03d2b727c44f8dc75bb980bdaaa6503c638c
Alex Hockley <ahockley@antivenom.socs.uoguelph.ca>
2016-04-01 16:46:58 -0400

    Added illumination and free, reworked some parameter passing and return values, just about ready to test it, just need to write the parser

--- A4/a.c
+++ A4/a.c
@@ -39,6 +39,30 @@ float viewHeight = 500;
 int rtX = 1024;
 int rtY = 768;
 
+
+//ambient light constants
+float IaR = 1.0; //ambient light source
+float IaG = 1.0;
+float IaB = 1.0;
+float Ka = 1.0; //ambient coefficient for objects
+
+
+//light constants
+float IpR = 1.0;
+float IpG = 1.0;
+float IpB = 1.0;
+
+//diffuse constants
+float Kd = 1.0;
+
+//specular constants
+float Ks = 1.0;
+float OsR = 1.0;
+float OsG = 1.0;
+float OsB = 1.0;
+float specN = 2.0;
+
+
 typedef struct Intersection{
   Point* in; //where the intersection goes into the object
   Point* out; //where the intersection leaves the object
@@ -48,7 +72,7 @@ typedef struct Intersection{
 }Intersection;
 
 typedef struct IntersectionInfo{
-  int numInterstections = rtX*rtY;
+  int numInterstections;
   Intersection** intersections;
 }IntersectionInfo;
 
@@ -75,6 +99,7 @@ typedef struct SceneInfo{
   LightInfo* light;
   SphereInfo** spheres;
   RayInfo* rays;
+  IntersectionInfo* intersectInfo;
 }SceneInfo;
 
 typedef struct LightInfo{
@@ -120,36 +145,82 @@ void init (void)
    glEnable(GL_DEPTH_TEST);
 }
 
+void freeScene(SceneInfo* scene)
+{
+    
+}
+
 void freeIntersections(IntersectionInfo* ii)
 {
-
+    if(ii == NULL)
+        return;
+    
+    if(ii->intersections != NULL){
+        for(int i = 0; i < ii->numIntersections; i++){
+            if(ii->intersections[i]->in != NULL)
+                free(ii->intersections[i]->in);
+            if(ii->intersections[i]->out != NULL)
+                free(ii->intersections[i]->out);
+            if(ii->intersections[i]->normalIn != NULL)
+                free(ii->intersections[i]->normalIn);
+            if(ii->intersections[i]->normalOut != NULL)
+                free(ii->intersections[i]->normalOut);
+            free(ii->intersections[i]);
+        }
+        free(ii->intersections);
+    }
+    free(ii);
+    ii = NULL;
 }
 
+//calculate the length of a vector as defined by starting and destination points
+void calculateLength(float sX, float sY, float sZ, float dX, float dY, float dZ)
+{
+    return abs(sqrt(pow(dX-sX,2) + pow(dY-sY,2) + pow(dZ-sZ,2)));
+}
+
+//normalize a vector and return the points back into pointers supplied for fX, fY, fZ
 void normalize(float sX, float sY, float sZ, float dX, float dY, float dZ, float* fX, float* fY, float fZ)
 {
-  float length = sqrt(pow(dX-sX,2) + pow(dY-sY,2) + pow(dZ-sZ,2));
-  fX = (dX-sX) / length;
-  fY = (dY-sY) / length;
-  fZ = (dZ-sZ) / length;
+  float length = calculateLength(sX, sY, sZ, dX, dY, dZ);
+  *fX = (dX-sX) / length;
+  *fY = (dY-sY) / length;
+  *fZ = (dZ-sZ) / length;
 }
 
+//calculate the distance between two points
 void calcDistance(Point* origin, Point* destination)
 {
   return sqrt(pow(destination->x - origin->x,2) + pow(destination->y - origin->y,2) + pow(destination->z - origin->z,2));
 }
 
-void calculateIntersections(SceneInfo* si, RayInfo* ri, IntersectionInfo* ii)
+//calculate the dot product of two points
+float dotProduct(Point* p1, Point* p2)
 {
-  if(ii != NULL){
-    freeIntersections(ii);
-  }
-  ii = (IntersectionInfo*)malloc(sizeof(IntersectionInfo));
-  ii->intersections = (Intersection**)malloc(sizeof(Intersection*)*ii->numIntersections);
-
-
-
+    return (p1->x*p1->y + p1->y*p2->y + p3->z*p3->z);
 }
 
+//calculate the intersections for each ray
+IntersectionInfo* calculateIntersections(SceneInfo* scene)
+{
+  IntersectionInfo* ii = (IntersectionInfo*)malloc(sizeof(IntersectionInfo));
+  ii->intersections = (Intersection**)malloc(sizeof(Intersection*)*ri->numRays);
+  ii->numIntersections = 0;
+
+  int intersectionNum = 0;
+    for(int i = 0; i < ri->totalRays; i++)
+    {
+        Intersection* inter = calculateIntersection(scene->spheres, scene->numSpheres, scene->rays[i]);
+        if(inter != NULL){
+            ii->intersections[intersectionNum] = inter;
+            ii->numIntersections++;
+            intersectionNum++;
+        }
+    }
+    return ii;
+}
+
+//calculate the intersection of a ray and the spheres
 Intersection* calculateIntersection(SphereInfo** spheres, int n, Ray* ray)
 {
 
@@ -165,6 +236,7 @@ Intersection* calculateIntersection(SphereInfo** spheres, int n, Ray* ray)
   float lastDistance = 0, tempDistance = 0;
 
 
+  //for each sphere, calculate the different components and store them in a temporary variable
   for(int i = 0; i < n; i++){
     Bt = 2*(ray->direction->point->x * (ray->origin->point->x * spheres[i]->location->x) +
            ray->direction->point->y * (ray->origin->point->y * spheres[i]->location->y) +
@@ -176,16 +248,17 @@ Intersection* calculateIntersection(SphereInfo** spheres, int n, Ray* ray)
     if((pow(Bt,2)-4*Ct) == 0) //no intersections
       continue;
     flag = 1;
-    //components of quadratic formula
+      
+    //determine the entry and exit t values using the quadratic formula
     Tot = -Bt - (sqrt(pow(Bt,2) - (4*Ct) )) / 2;
     Tft = -Bt + (sqrt(pow(Bt,2) - (4*Ct) )) / 2;
 
-    //see if the entry is closer to the camera than the last one
+    //calculate the point of the entry in the world
     tempPoint->x = ray->origin->x + ray->direction->x*Tot;
     tempPoint->y = ray->origin->y + ray->direction->y*Tot;
     tempPoint->z = ray->origin->z + ray->direction->z*Tot;
 
-    //if it is, replace the A B C To Td values with the last calculated ones and continue
+    //check it is closer to the camera than the last intersection if it is, replace the A B C To Td values with the last calculated ones and continue
     tempDistance = calcDistance(tempPoint, ray->origin);
     if(firstFlag == 1 || abs(tempDistance) < abs(lastDistance)){
       firstFlag = 0;
@@ -198,37 +271,119 @@ Intersection* calculateIntersection(SphereInfo** spheres, int n, Ray* ray)
     }
 
   }
-  if(flag == 1){ //we had an intersection
+  //we had an intersection
+  if(flag == 1){
+    //allocate memory and assign values of the intersection
     inter = (Intersection*) malloc(sizeof(Intersection))
+      
+    //entry intersection point
     inter->in = (Point*)malloc(sizeof(Point));
     inter->in->x = ray->origin->x + ray->direction->x*To;
     inter->in->y = ray->origin->y + ray->direction->y*To;
     inter->in->z = ray->origin->z + ray->direction->z*To;
 
+    //exit intersection point
     inter->out = (Point*)malloc(sizeof(Point));
     inter->out->x = ray->origin->x + ray->direction->x*Tf;
     inter->out->y = ray->origin->y + ray->direction->y*Tf;
     inter->out->z = ray->origin->z + ray->direction->z*Tf;
 
+    //normal direction vector for the entry point
     inter->normalIn = (Point*)malloc(sizeof(Point));
     inter->normalIn->x = (inter->in->x - spheres[sphereNum]->location->x) / spheres[sphereNum]->radius;
     inter->normalIn->y = (inter->in->y - spheres[sphereNum]->location->y) / spheres[sphereNum]->radius;
     inter->normalIn->z = (inter->in->z - spheres[sphereNum]->location->z) / spheres[sphereNum]->radius;
 
+    //normal direction vector for the exit point (may be pointless)
     inter->normalOut = (Point*)malloc(sizeof(Point));
     inter->normalOut->x = (inter->out->x - spheres[sphereNum]->location->x) / spheres[sphereNum]->radius;
     inter->normalOut->y = (inter->out->y - spheres[sphereNum]->location->y) / spheres[sphereNum]->radius;
     inter->normalOut->z = (inter->out->z - spheres[sphereNum]->location->z) / spheres[sphereNum]->radius;
 
-    inter->sphereNum = sphereNum; //store which sphere we intersected
+    //store which sphere we intersected, we will need this later
+    inter->sphereNum = sphereNum;
   }
+  //free up the temporary point
   free(tempPoint);
   return inter;
 }
 
+Point* calculateReflectionVector (Point* normal, Point* lightVector)
+{
+    Point* r = (Point*)malloc(sizeof(Point));
+    //float angle = 0;
+    
+    /*angle = acos(dotProduct(normal,lightVector) / (calculateLength(intersection->x, intersection->y, intersection->z, normal->x, normal->y, normal->z) * calculateLength(intersection->x, intersection->y, intersection->z, lightVector->x, lightVector->y, lightVector->z));*/
+    
+    float dot = dotProduct(lightVector, normal);
+    
+    r->x = lightVector->x - 2 * dot * normal->x;
+    r->y = lightVector->y - 2 * dot * normal->y;
+    r->y = lightVector->z - 2 * dot * normal->z;
+    
+}
+
+
+void illuminate(SceneInfo* scene)
+{
+    float r, g ,b;
+    Point* diffuseLightVector = (Point*)malloc(sizeof(Point));
+    Point* specularViewVector = (Point*)malloc(sizeof(Point));
+    Point* specularReflectVector = NULL;
+    
+    
+    
+    for(int i = 0; i < scene->intersectInfo->numIntersections; i++){
+        
+        //calculate the vectors
+        //diffuse
+        normalize(scene->intersectInfo->intersections[i]->in->x, scene->intersectInfo->intersections[i]->in->y, scene->intersectInfo->intersections[i]->in->z,
+                  sceneInfo->light->x, sceneInfo->light->y, sceneInfo->light->z,
+                  &diffuseLightVector->x, &diffuseLightVector->y, &diffuseLightVector->z);
+        
+        //specular view
+        normalize(scene->intersectInfo->intersections[i]->in->x, scene->intersectInfo->intersections[i]->in->y, scene->intersectInfo->intersections[i]->in->z,
+                  camX, camY, camZ,
+                  &speculuarViewVector->x, &specularViewVector->y, &specularViewVector->z);
+        
+        //specular reflection
+        specularReflectVector = calculateReflectionVector(scene->intersectInfo->intersections[i]->normalIn, scene->light);
+        
+        //calculate r ambient, diffuse and specular combination colour
+        r = IaR * Ka * scene->spheres[scene->intersectInfo->intersections[i]->sphereNum]->r +
+            IpR * (Kd * scene->spheres[scene->intersectInfo->intersections[i]->sphereNum]->r * dotProduct(scene->intersectInfo->intersections[i]->normalIn, diffuseLightVector) +
+                   Ks * KsR * pow(dotProduct(specularReflectVector, specularViewVector), specN));
+        
+        //calculate g ambient, diffuse and
+        g = IaG * Ka * scene->spheres[scene->intersectInfo->intersections[i]->sphereNum]->g +
+            IpG * (Kd * scene->spheres[scene->intersectInfo->intersections[i]->sphereNum]->g * dotProduct(scene->intersectInfo->intersections[i]->normalIn, diffuseLightVector) +
+                   Ks * KsG * pow(dotProduct(specularReflectVector, specularViewVector), specN));
+
+        
+        //calculate b
+        b = IaB * Ka * scene->spheres[scene->intersectInfo->intersections[i]->sphereNum]->b +
+            IpB * (Kd * scene->spheres[scene->intersectInfo->intersections[i]->sphereNum]->b * dotProduct(scene->intersectInfo->intersections[i]->normalIn, diffuseLightVector) +
+                   Ks * KsB * pow(dotProduct(specularReflectVector, specularViewVector),specN));
+
+        
+        free(specularReflectVector);
+        specularReflectVector = NULL;
+        
+        //fill pixel with rgb value at desired location
+                   
+    }
+    
+    //free the temporary calculation vectors
+    free(diffuseLightVector);
+    free(specularViewVector);
+    free(specularReflectVector);
+    
+    
+}
 
 void drawScene(SceneInfo* scene)
 {
+    illuminate(scene);
 }
 
 void setupOriginVectors(RayInfo* ri)
@@ -253,6 +408,20 @@ void setupDirectionVectors(RayInfo* ri)
       curRay++;
     }
   }
+}
+
+RayInfo* calculateRays(SceneInfo* scene)
+{
+    RayInfo* rays = (RayInfo*)malloc(sizeof(RayInfo));
+    rays->rays = (Ray**)malloc(sizeof(Ray*)*rays->numRays);
+    for(int i = 0; i < rays->numRays; i++){
+        rays->rays[i] = (Ray*)malloc(sizeof(Ray));
+        rays->rays[i]->origin = (Point*)malloc(sizeof(Point));
+        rays->rays[i]->direction = (Point*)malloc(sizeof(Point));
+    }
+    setupOriginVectors(rays);
+    setupDirectionVectors(rays);
+    return rays;
 }
 
 void display (void)
@@ -392,7 +561,8 @@ int main(int argc, char** argv)
    glutCreateWindow (argv[0]);
 
    sceneInfo = loadScene(argv[1]);
-   sceneInfo->rays = calculateRays(sceneInfo->light);
+   sceneInfo->rays = calculateRays(sceneInfo);
+   sceneInfo->intersectionInfo = calculateIntersections(sceneInfo);
 
    init();
    glutReshapeFunc (reshape);
